7127e11a2f443d846e60c671490b8a1b0076d1ba
Ashwin <ashwankumar555@gmail.com>
2025-08-31 00:13:46 +0530

    new

---
+++ src/test/java/AllureReports/allure/CucumberRunnerTests.java
@@ -0,0 +1,12 @@
+package AllureReports.allure;
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+ 
+@RunWith(Cucumber.class)
+@CucumberOptions(tags = "", features = {"src/test/resources/features"}, glue = {"AllureReports.allure.definitions"},
+        plugin = {"pretty","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"})
+ 
+public class CucumberRunnerTests  {
+ 
+}
