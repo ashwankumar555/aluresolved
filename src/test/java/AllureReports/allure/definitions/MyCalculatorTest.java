65773a45799d9b2118ee128a55da18ac0437d336
Ashwin <ashwankumar555@gmail.com>
2025-08-31 10:49:12 +0530

    jjjj

--- src/test/java/AllureReports/allure/definitions/MyCalculatorTest.java
+++ src/test/java/AllureReports/allure/definitions/MyCalculatorTest.java
@@ -3,8 +3,15 @@ package AllureReports.allure.definitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Link;
+import io.qameta.allure.junit4.DisplayName;
+
 import org.junit.Assert;
 
+@Epic("All test cases")
 public class MyCalculatorTest {
 
     double sum;  // shared instance variable
@@ -14,12 +21,16 @@ public class MyCalculatorTest {
         System.out.println("Starting addition scenario");
     }
 
+    @Feature("All calculations")
+    @DisplayName("Calculate the sum")
+    @Description("This method adds two numbers")
     @When("User enters n1 as {double} and n2 as {double}")
     public void user_enters_numbers(double n1, double n2) {
         sum = n1 + n2;
         System.out.println("Sum calculated: " + sum);
     }
 
+    @Link("www.google.com")
     @Then("Addition result should be {double}")
     public void addition_should_be(double expectedSum) {
         Assert.assertEquals(expectedSum, sum, 0.001);
